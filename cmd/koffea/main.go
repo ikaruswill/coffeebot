f1dca3d991067d2155918ba13e7ff92c4203a2a4
Will Ho <ikaruswill.ho@gmail.com>
2021-07-11 01:56:05 +0800

    Add listgb

--- cmd/koffea/main.go
+++ cmd/koffea/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/ikaruswill/koffea/internal/client/sqlite"
 	"github.com/ikaruswill/koffea/internal/client/telegram"
 	"github.com/ikaruswill/koffea/internal/config"
-	"github.com/ikaruswill/koffea/internal/storage/user"
+	groupbuyStorage "github.com/ikaruswill/koffea/internal/storage/groupbuy"
+	orderStorage "github.com/ikaruswill/koffea/internal/storage/order"
+	userStorage "github.com/ikaruswill/koffea/internal/storage/user"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
@@ -39,26 +42,46 @@ func main() {
 	var dbClient *sqlite.Client
 	dbClient, err = sqlite.NewConnection(koffeaConfig.Storage.Sqlite)
 
+	dbClient.DB.AutoMigrate(
+		&userStorage.User{},
+		&orderStorage.Order{},
+		&groupbuyStorage.GroupBuy{},
+	)
+
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
 	tClient.Bot.Handle("/start", func(m *tb.Message) {
-		tClient.Bot.Send(m.Sender, "Hi! My name is Koffea, how can I help you today?")
-		tClient.Bot.Send(m.Sender, `/listgb - List active group buys
-		/viewgb - View information about a group buy
-		/startgb - Start a group buy
-		/joingb - Join a group buy
-		/endgb - End a group buy`)
-
-		dbClient.DB.Create(&user.User{
+		user := &userStorage.User{
 			FirstName:    m.Sender.FirstName,
 			LastName:     m.Sender.LastName,
 			Username:     m.Sender.Username,
 			LanguageCode: m.Sender.LanguageCode,
 			IsBot:        m.Sender.IsBot,
-		})
+		}
+		dbClient.DB.FirstOrCreate(&user)
+
+		tClient.Bot.Send(m.Sender, fmt.Sprintf("Hi %s! My name is Koffea, how can I help you today?", user.FirstName))
+		tClient.Bot.Send(m.Sender, `/listgb - List active group buys
+		/viewgb - View information about a group buy
+		/startgb - Start a group buy
+		/joingb - Join a group buy
+		/endgb - End a group buy`)
+	})
+
+	tClient.Bot.Handle("/listgb", func(m *tb.Message) {
+		var groupBuys []groupbuyStorage.GroupBuy
+		dbClient.DB.Order("created_at desc").Find(&groupBuys)
+
+		response := "Here are the current active group buys:\n"
+		for idx, groupBuy := range groupBuys {
+			user := &userStorage.User{}
+			dbClient.DB.Where("id = ?", groupBuy.ID).First(&user)
+			response += fmt.Sprintf("%d. [%s] %s by %s %s", idx+1, groupBuy.CreatedAt.Format("2006-01-02"), groupBuy.Name, user.FirstName, user.LastName)
+		}
+		tClient.Bot.Send(m.Sender, response)
 	})
 
 	tClient.Bot.Start()
