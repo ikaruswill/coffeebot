61ac8f243ceef8defa6c1d56dc8e8ab2dd3f608c
Will Ho <ikaruswill.ho@gmail.com>
2021-07-10 18:56:46 +0800

    Enrich User model with Telegram metadata

--- internal/storage/user/model.go
+++ internal/storage/user/model.go
@@ -6,9 +6,14 @@ import (
 	"gorm.io/gorm"
 )
 
+// User stores Telegram user metadata
 type User struct {
 	gorm.Model
+	FirstName    string `gorm:"not null"`
+	LastName     string `gorm:"not null"`
 	Username     string `gorm:"not null"`
+	LanguageCode string
+	IsBot        bool
 	GroupBuys    []groupbuy.GroupBuy
 	Orders       []order.Order
 }
