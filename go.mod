a94ec582a0a9b9c2d792122623522aa00d5314da
Will Ho <ikaruswill.ho@gmail.com>
2021-07-10 20:04:15 +0800

    Implement config

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	gopkg.in/tucnak/telebot.v2 v2.3.5
+	gopkg.in/yaml.v2 v2.2.2
+	gorm.io/driver/postgres v1.1.0
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.11
 )
---
+++ internal/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/ikaruswill/koffeabot/internal/client/postgres"
+	"github.com/ikaruswill/koffeabot/internal/client/sqlite"
+	"github.com/ikaruswill/koffeabot/internal/client/telegram"
+	"gopkg.in/yaml.v2"
+)
+
+type StorageDriver string
+
+const (
+	SqliteStorageDriver   = "sqlite"
+	PostgresStorageDriver = "postgres"
+)
+
+type StorageConfig struct {
+	Driver   StorageDriver   `yaml:"driver"`
+	Sqlite   sqlite.Config   `yaml:"sqlite"`
+	postgres postgres.Config `yaml:"postgres"`
+}
+
+type Config struct {
+	Telegram telegram.Config `yaml:"telegram"`
+	Storage  StorageConfig   `yaml:"storage"`
+}
+
+func (c *Config) Load(path string) error {
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+		return err
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+		return err
+	}
+	return nil
+}
